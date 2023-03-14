b98ec773d1dd60937b460c5e08812ca56036082e
Carlos Fraga <vittorfraga@outlook.com>
2023-03-14 02:21:36 -0300

    ajustando o dataSource

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "testedb",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432"),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
   entities: [`${__dirname}/../database/entities/*{.ts,.js}`],
